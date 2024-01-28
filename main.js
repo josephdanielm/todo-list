1851cf33f62425d3408e5a9d7fbe33d08152f4c9
Joseph Mudd <josephmuddjr@gmail.com>
2024-01-28 13:31:03 -0600

    Create project module with some methods

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
 import './style.css';
 import Todo from './todo.js';
+import Project from './project.js';
 
-const todo1 = new Todo('tit', 'desc', '010203', 'high');
+const defaultTodo = new Todo('Default Todo', 'Description for this Todo goes here!', '01-01-2000', 'high');
+const defaultProject = new Project('Default Project');
+
+
+defaultProject.addTodo(defaultTodo);
+
+
+
+console.table(defaultTodo);
+console.table(defaultProject);
--- project.js
+++ project.js
@@ -1,5 +1,29 @@
 export default class Project {
     constructor(name) {
+        this.name = name;
+        this.todoList = [];
+    }
 
+    addTodo(todo) {
+        this.todoList.push(todo);
+    }
+
+    removeTodo(todo) {
+        const todoIndex = this.todos.indexOf(todo);
+        if (todoIndex !== -1) {
+            this.todos.splice(todoIndex, 1);
+        }
+    }
+
+    getTodoList() {
+        return this.todoList;
+    }
+
+    getCompletedTodos() {
+        return this.todoList.filter(todo => todo.completed);
+    }
+
+    getPendingTodos() {
+        return this.todoList.filter(todo => !todo.completed);
     }
 }
