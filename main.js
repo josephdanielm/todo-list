f60bba5dee97d0de1396a0a74c2e4ecad175e7bd
Joseph Mudd <josephmuddjr@gmail.com>
2024-01-28 12:34:04 -0600

    Create Todo constructor with basic methods

--- main.js
+++ main.js
@@ -1,2 +1,4 @@
-import './style.css'
+import './style.css';
+import Todo from './todo.js';
 
+const todo1 = new Todo('tit', 'desc', '010203', 'high');
---
+++ todo.js
@@ -0,0 +1,21 @@
+export default class Todo {
+    constructor(title, description, dueDate, priority) {
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.completed = false;
+    }
+
+    markCompleted() {
+        this.completed = true;
+        console.log(`${this.title} has been completed`);
+    }
+
+    editTodo(newTitle, newDescription, newDueDate, newPriority) {
+        this.title = newTitle;
+        this.description = newDescription;
+        this.dueDate = newDueDate;
+        this.priority = newPriority;
+    }
+}
